2011ca14b92f8b2bf213fad8caf8fe44219a1f27
Julian Florez <forezjuli08@hotmail.com>
2023-03-17 08:15:40 -0500

    first commit initial config & first server with express

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hellooooo')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+//definir ruta
+app.get('/', (req, res) => {
+  res.send('hola, mi servidor en express')
+})
+
+//decirle que escuche en un puerto especifico
+app.listen(port, () => {
+  console.log('Escuchando en el puerto', port)
+})
