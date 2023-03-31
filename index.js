52f47ff9bea01bef3d60a819f09eefe0a51d9d8f
Julian Florez <forezjuli08@hotmail.com>
2023-03-31 07:54:04 -0500

    06 - Get params

--- index.js
+++ index.js
@@ -15,9 +15,34 @@ app.get('/nueva-ruta', (req, res) => {
 
 //enviar JSON como respuesta
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'product 1',
+      price: 500,
+    },
+    {
+      name: 'product 2',
+      price: 2000,
+    },
+  ])
+})
+
+// dos puntos para decir que es un parámetro
+// en este caso va a devolver el producto 1 con el id que le pasemos (hardcodeado, quemado)
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
   res.json({
+    id,
     name: 'product 1',
     price: 500,
+  })
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+  res.json({
+    categoryId,
+    productId,
   })
 })
 
