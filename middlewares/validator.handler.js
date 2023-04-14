facd661704117debfa0a79637eaa395d963f51c4
Julian Florez <forezjuli08@hotmail.com>
2023-04-14 07:51:55 -0500

    20 - validate data using Joi

---
+++ middlewares/validator.handler.js
@@ -0,0 +1,21 @@
+const boom = require('@hapi/boom')
+
+// configuramos un middleware que sea dinámico
+function validatorHandler(schema, property) {
+  console.log('validatorHandler')
+
+  // creador de un middleware
+  return (req, res, next) => {
+    const data = req[property] // req.body | req.params | req.query
+    const { error } = schema.validate(data)
+
+    if (error) {
+      next(boom.badRequest(error))
+    }
+    next()
+  }
+}
+
+module.exports = validatorHandler
+
+useState<Record<String,String>>
